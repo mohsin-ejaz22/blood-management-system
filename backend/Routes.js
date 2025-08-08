7a33411dde43706e58eb0f4d66eb963d2a8c5dfd
mustu633 <mustafakamal63633@gmail.com>
2025-08-07 19:32:20 -0700

    setup user request controller and its routes

--- backend/Routes.js
+++ backend/Routes.js
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import UserController from "./controller/UserController.js";
+import userRequestController from "./controller/userRequestController.js";
 
 
 const router = Router();
 
-//for User
+//for User:
 router.post("/user/create", UserController.createUser);
 router.get("/users", UserController.showUsers);
 router.get("/user/:id", UserController.showUser);
 router.post("/user/edit/:id", UserController.editUser);
 router.post("/user/delete/:id", UserController.deleteUser);
+
+//for UserRequest:
+router.post("/users/:id/userrequests/:user_ID", userRequestController.createuserrequest);
+router.delete("/users/:id/userrequests/:userrequestId", userRequestController.deleteuserrequest);
 
 export default router;
---
+++ backend/controller/userRequestController.js
@@ -0,0 +1,85 @@
+import { ObjectId } from "mongodb";
+import User from "../models/UserModel.js";
+import Userrequest from "../models/UserRequestModel.js";
+
+export default {
+  // for creating new request:
+  async createuserrequest(req, res) {
+    const { id, user_ID } = req.params;
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid User ID" });
+    }
+
+    const {
+      Patientname,
+      age,
+      bloodgroup,
+      disease,
+      doctorname,
+      address,
+      mobile,
+    } = req.body;
+
+    const profilepic = req.files ? req.files.img : null;
+    const profilepicPath = img
+      ? `uploads/${Date.now()}${path.extname(img.name)}`
+      : null;
+
+    try {
+      const user = await User.findById(id);
+
+      const newUserRequest = await Userrequest({
+        Patientname,
+        age,
+        bloodgroup,
+        disease,
+        doctorname,
+        address,
+        mobile,
+        profilepic:profilepicPath,
+      });
+
+      user.userrequests.push(newUserRequest);
+
+      await newUserRequest.save();
+      await user.save();
+      if (profilepic) {
+        await profilepic.mv(profilepicPath);
+      }
+
+      return res.status(201).json(newUserRequest);
+    } catch (error) {
+      console.log(error);
+      if (error.name === "ValidationError") {
+        const errors = Object.values(error.errors).map((err) => ({
+          msg: err.message,
+          path: err.path,
+        }));
+
+        return res.status(400).json({ errors });
+      }
+      return res.status(500).json({ errors: [{ msg: error.message }] });
+    }
+  },
+// .............
+  //   // For deleting userrequest:
+  async deleteuserrequest(req, res) {
+    const { id, userrequestId } = req.params;
+
+    if (!ObjectId.isValid(id)) {
+      res.status(400).json({ message: "Your user id is invalid!" });
+    }
+    if (!ObjectId.isValid(userrequestId)) {
+      res.status(400).json({ message: "Your userrequest id is invalid!" });
+    }
+
+    try {
+      await User.findByIdAndUpdate(id, { $pull: { userrequests: userrequestId } });
+      await Userrequest.findByIdAndDelete(userrequestId);
+      return res.status(200).json({ message: "Userrequest deleted successfully!" });
+    } catch (error) {
+      console.log("error in deleting Userrequest: ", error);
+    }
+  },
+};
